2d941386bb1d63f6358a97c0e80068584d3d470b
Sanjay Yadav <sanjayyadavsky57@gmail.com>
2023-09-27 23:26:03 +0545

    commit all

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const jsonfile = require('jsonfile')
-
 const moment = require('moment')
 
-const FILE_PATH = '/data.json'
-
+const FILE_PATH = "/test.js"
 const DATE = moment().format();
-
 const data = {
     date:DATE
 }
-
-jsonefile.writeFile(FILE_PATH,)
+console.log(moment)
+// jsonfile.writeFile(FILE_PATH)
